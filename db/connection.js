91acf1e76ef4f2b74b98b7e2daa56ab397ad7c8e
kevinjbutler1994 <kevinjbutler1994@gmail.com>
2024-03-13 09:48:54 -0400

    commits

--- db/connection.js
+++ db/connection.js
@@ -1,29 +1,24 @@
-import mongoose from 'mongoose'
-import chalk from 'chalk';
-connectionString = process.env.DB_URL || 'mongodb://localhost/bookmarks'
+import mongoose from "mongoose";
 
+const connectionString =
+  process.env.DB_URL || "mongodb://127.0.0.1:27017/express-project-api";
+
+// This is needed if you use Model.findByIdAndUpdate method, specifically so that {new: true} is the default
 mongoose.set("returnOriginal", false);
 
-mongoose.connect("mongodb://127.0.0.1:27017/starwars_api").catch((err) => {
-    console.log(`Error connection to MongoDB: ${err.message}`);
-});
+mongoose
+  .connect(connectionString)
+  .catch((error) =>
+    console.log("Error connecting to MongoDB: ", error.message)
+  );
 
-// line 6 star wars api is the portion needed to swap out with new APIs, all other connection.js code remains unchanged 
-// 
+mongoose.connection.on("disconnected", () =>
+  console.log(chalk.bold("Disconnected from MongoDB!"))
+);
 
-// *************************
-// Ask about line 6 in case of debugging, the starwars_api section likely will 
-// have played a role**************************8
-// ***********************8
-// **********************
+mongoose.connection.on("error", (error) =>
+  console.error(chalk.red(`MongoDB connection error: ${error}`))
+);
 
-mongoose.connection.on("disconnect", ()=> {
-    console.log("disconnected")
-})
- mongoose.connection.on("error", (err) => {
-    console.log(chalk.bold('MongoDB connection error: ${err}'));
- });
-
-
-export default mongoose.connection
+export default mongoose.connection;
 
